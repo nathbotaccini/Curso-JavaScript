d34a1964183b737250ef798c9e8ec847cc44dc56
nathbotaccini <nathaliabotaccini@gmail.com>
2020-05-17 22:00:44 -0300

    commit 17/05/2020

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+
+/* Melhor Solução
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula16/função01.js
@@ -0,0 +1,9 @@
+function parímpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+console.log(parímpar(223))
